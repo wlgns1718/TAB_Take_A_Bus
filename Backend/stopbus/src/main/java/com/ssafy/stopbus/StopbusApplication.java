e19aa1a21310cf625f3b8965417363131bac69c5
mincheolsong <thdalscjf05@naver.com>
2023-07-20 17:17:32 +0900

    REFATOR : StopbusApplication.class

--- Backend/stopbus/src/main/java/com/ssafy/stopbus/StopbusApplication.java
+++ Backend/stopbus/src/main/java/com/ssafy/stopbus/StopbusApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StopbusApplication {
 
 	public static void main(String[] args) {
-		System.out.print("여기 들어온다");
 
 		SpringApplication.run(StopbusApplication.class, args);
 	}
