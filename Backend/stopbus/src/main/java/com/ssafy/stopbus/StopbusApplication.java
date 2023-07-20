8c9259c4df74b7d495f61af63ff91ab770be0598
DESKTOP-DOGVPUB\SSAFY <ssafy>
2023-07-20 17:02:56 +0900

    add: init setting

---
+++ Backend/stopbus/src/main/java/com/ssafy/stopbus/StopbusApplication.java
@@ -0,0 +1,15 @@
+package com.ssafy.stopbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StopbusApplication {
+
+	public static void main(String[] args) {
+		System.out.print("여기 들어온다");
+
+		SpringApplication.run(StopbusApplication.class, args);
+	}
+
+}
---
+++ Backend/stopbus/src/test/java/com/ssafy/stopbus/StopbusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.stopbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StopbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
