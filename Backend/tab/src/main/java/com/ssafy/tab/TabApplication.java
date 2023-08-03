0ca872d8d010302a1911741a194352a6f5461f0f
성연석 <abcd9351@naver.com>
2023-08-03 15:36:21 +0900

    Feat: change bus table

--- Backend/tab/src/main/java/com/ssafy/tab/TabApplication.java
+++ Backend/tab/src/main/java/com/ssafy/tab/TabApplication.java
@@ -8,6 +8,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TabApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TabApplication.class, args);
-
 	}
 }
