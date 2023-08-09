5ba405c31e1b7adff7598f20ab4b27299b1c5e26
mincheolsong <thdalscjf05@naver.com>
2023-08-09 22:16:47 +0900

    feat : add requestToken

--- Backend/tab/src/main/java/com/ssafy/tab/repository/UserRepository.java
+++ Backend/tab/src/main/java/com/ssafy/tab/repository/UserRepository.java
@@ -21,7 +21,8 @@ public interface UserRepository extends JpaRepository<User,Long> {
     List<User> findByUserId(String userId);
     Optional<User> findById(Long id); // 단건 Optional
 
-    /*public UserDto loginUser(UserDto userDto) {
-        
-    }*/
+    @Query("select u.refreshToken from User u where u.userId = :userId")
+    Optional<String> findRefreshToken(@Param("userId") String userId);
+
+
 }
