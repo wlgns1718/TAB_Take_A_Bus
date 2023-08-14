6bcb5b66d0fa29439e368d20c135022eb4161170
JeHyeong <tlswpgud22@nate.com>
2023-08-14 09:13:21 +0900

    Go master

--- Frontend/src/store/api/api.ts
+++ Frontend/src/store/api/api.ts
@@ -2,20 +2,20 @@ import axios from "axios";
 
 
 export const arduinoAPI = axios.create({
-  baseURL: "http://i9d111.p.ssafy.io:8000/tab/arduino/",
+  baseURL: "https://i9d111.p.ssafy.io:8000/tab/arduino/",
 });
 export const busAPI = axios.create({
-  baseURL: "http://i9d111.p.ssafy.io:8000/tab/station/",
+  baseURL: "https://i9d111.p.ssafy.io:8000/tab/station/",
 });
 export const noticeAPI = axios.create({
-  baseURL: "http://i9d111.p.ssafy.io:8000/tab/notice/",
+  baseURL: "https://i9d111.p.ssafy.io:8000/tab/notice/",
   // baseURL: "http://localhost:8000/notice/",
 });
 export const boardAPI = axios.create({
   // baseURL: "https://i9d111.p.ssafy.io:8001/notice/",
-  baseURL: "http://i9d111.p.ssafy.io:8000/tab/board",
+  baseURL: "https://i9d111.p.ssafy.io:8000/tab/board",
 });
 export const webAPI = axios.create({
-  baseURL: "http://i9d111.p.ssafy.io:8000/tab/"
+  baseURL: "https://i9d111.p.ssafy.io:8000/tab/"
 })
 
